053ce0689c985acfc586c9e8c28136ff9106e4a0
Laura Buzek <lbuzek@gmail.com>
2018-08-06 22:00:24 -0600

    Changed implementation of main function, don't want to use switch, doesn't work for majority of columns

--- normalize_csv.py
+++ normalize_csv.py
@@ -13,32 +13,37 @@ def main():
 	csv_contents = csv.DictReader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL)
 	data = list(csv_contents)
 	for row in data:
+		#Initialize duration as not updated, we only want to handle once
+		updatedDuration = False
 		for (name, value) in row.items():
 			value = value.decode("utf-8", "replace")
-			#will add try statement here
-			validation_fn = get_validation_fns(name, value)
-			result = None
-			if validation_fn == 'noop':
-				result = value
-			else:
-				result = validation_fn(value)
+			#TODO: will add try statement here
+			
+			#Check for simple functions to run
+			fn = None
+			if name == 'Timestamp':
+				fn = format_timestamp
+			elif name == 'ZIP':
+				fn = format_zipcode
+			elif name == 'FullName':
+				fn = format_name
+			
+			#If duration, handle all at once, otherwise run fn
+			result = value
+			if name.endswith("Duration") and not updatedDuration:
+				#run the duration function
+				result = update_duration(value)
+				#Flag the duration as updated
+				updatedDuration = True
+			elif fn != None:
+				result = fn(value)
+
 			row[name] = result
 			
+		#TODO: will remove
 		print row
 			
 
-def get_validation_fns(header, value):
-	names_to_fns = {
-		'Timestamp': format_timestamp,
-		'ZIP': format_zipcode,
-		'FullName': format_name,
-		'FooDuration': format_duration,
-		'BarDuration': format_duration,
-		'TotalDuration': update_total_duration
-	}
-	#Note and Address are noops here
-	fn = names_to_fns.get(header, "noop")
-	return fn
 		
 def format_timestamp(ts):
 	ts_as_date_et = datetime.strptime(ts, "%m/%d/%y %H:%M:%S %p")
@@ -54,21 +59,14 @@ def format_zipcode(z):
 def format_name(name):
 	return name.upper()
 
-def format_duration(dur):
-
-	return dur
-#The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
-  #format (where MS is milliseconds); please convert them to a floating
-  #point seconds format.
-
-def update_total_duration(dur):
+def update_duration(dur):
+	#TODO: implement
 	return dur
 	#The column "TotalDuration" is filled with garbage data. For each
  # row, please replace the value of TotalDuration with the sum of
   #FooDuration and BarDuration.
 
-def noop(rep):
-	return
+
 	#"a\x81b".decode("utf-8", "replace")
 	#The column "Notes" is free form text input by end-users; please do
   #not perform any transformations on this column. If there are invalid
