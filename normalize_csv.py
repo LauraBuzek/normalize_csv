9763b0a6f73b99d56b67620ca64b1f8d1f5b36f8
Laura Buzek <lbuzek@gmail.com>
2018-08-06 21:47:15 -0600

    Added name calculations, modified function-getter

--- normalize_csv.py
+++ normalize_csv.py
@@ -16,31 +16,33 @@ def main():
 		for (name, value) in row.items():
 			value = value.decode("utf-8", "replace")
 			#will add try statement here
-			result = execute_validation_fns(name, value)
+			validation_fn = get_validation_fns(name, value)
+			result = None
+			if validation_fn == 'noop':
+				result = value
+			else:
+				result = validation_fn(value)
 			row[name] = result
 			
 		print row
 			
 
-
-def execute_validation_fns(header, value):
+def get_validation_fns(header, value):
 	names_to_fns = {
 		'Timestamp': format_timestamp,
-		'Address': format_address,
 		'ZIP': format_zipcode,
 		'FullName': format_name,
 		'FooDuration': format_duration,
 		'BarDuration': format_duration,
-		'TotalDuration': update_total_duration,
-		'Note': noop #this won't work, need a fn
+		'TotalDuration': update_total_duration
 	}
-	fn = names_to_fns.get(header, noop)
-	return fn(value)
+	#Note and Address are noops here
+	fn = names_to_fns.get(header, "noop")
+	return fn
 		
 def format_timestamp(ts):
 	ts_as_date_et = datetime.strptime(ts, "%m/%d/%y %H:%M:%S %p")
 	ts_as_date_pt = ts_as_date_et - timedelta(hours=3)
-
 	return datetime.strftime(ts_as_date_pt, "%m/%d/%y %H:%M:%S %p")
   	
 def format_zipcode(z):
@@ -48,27 +50,19 @@ def format_zipcode(z):
 		missing = 5 - len(z)
 		z = "0"*missing + z
 	return z
-	#All ZIP codes should be formatted as 5 digits. If there are less
-  #than 5 digits, assume 0 as the prefix.
 
 def format_name(name):
-	return
-	#All name columns should be converted to uppercase. There will be
-  #non-English names.
-
-def format_address(addr):
-	return
-	#The Address column should be passed through as is, except for
-  #Unicode validation. 
+	return name.upper()
 
 def format_duration(dur):
-	return
+
+	return dur
 #The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
   #format (where MS is milliseconds); please convert them to a floating
   #point seconds format.
 
 def update_total_duration(dur):
-	return
+	return dur
 	#The column "TotalDuration" is filled with garbage data. For each
  # row, please replace the value of TotalDuration with the sum of
   #FooDuration and BarDuration.
