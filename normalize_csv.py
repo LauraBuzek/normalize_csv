be1ef3456a022c48472c81173ee63bc5b591dd27
Laura Buzek <lbuzek@gmail.com>
2018-08-06 20:33:53 -0600

    Added utf-decoding, encoding, switch fn for header -> fn mapping

--- normalize_csv.py
+++ normalize_csv.py
@@ -9,12 +9,37 @@ import csv
 def main():
 	filename = sys.argv[1]
 	f = open(filename, 'r')
-	csv_contents = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL)
+	csv_contents = csv.DictReader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL)
 	data = list(csv_contents)
 	for row in data:
+		for (name, value) in row.items():
+			value = value.decode("utf-8", "replace")
+			#will add try statement here
+			#result = execute_validation_fns(name, value)
+			value = value.encode('utf-8')
+			row[name] = value
+			
 		print row
+			
+
+
+def execute_validation_fns(header, value):
+	names_to_fns = {
+		'Timestamp': format_timestamp,
+		'Address': format_address,
+		'ZIP': format_zipcode,
+		'FullName': format_name,
+		'FooDuration': format_duration,
+		'BarDuration': format_duration,
+		'TotalDuration': update_total_duration,
+		'Note': unicode_replacement #this won't work
+	}
+
+	fn = names_to_fns.get(header, "Invalid header")
+	return fn(value)
 		
 def format_timestamp():
+
 	return
 	#The Timestamp column should be formatted in ISO-8601 format.
 	#date.isoformat()
@@ -41,6 +66,25 @@ def format_address():
 	#The Address column should be passed through as is, except for
   #Unicode validation. 
 
+def format_duration():
+	return
+#The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
+  #format (where MS is milliseconds); please convert them to a floating
+  #point seconds format.
+
+def update_total_duration():
+	return
+	#The column "TotalDuration" is filled with garbage data. For each
+ # row, please replace the value of TotalDuration with the sum of
+  #FooDuration and BarDuration.
+
+def unicode_replacement():
+	return
+	#"a\x81b".decode("utf-8", "replace")
+	#The column "Notes" is free form text input by end-users; please do
+  #not perform any transformations on this column. If there are invalid
+  #UTF-8 characters, please replace them with the Unicode Replacement
+  #Character.
 
 
 
