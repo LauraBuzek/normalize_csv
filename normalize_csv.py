09076910e25e2d6f544a1abb40ead3b124789990
Laura Buzek <lbuzek@gmail.com>
2018-08-06 21:14:36 -0600

    Added timestamp and zipcode fixes

--- normalize_csv.py
+++ normalize_csv.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+from datetime import datetime, timedelta
 
 #Please note there are commas in the Address
   #field; your CSV parsing will need to take that into account. Commas
@@ -15,9 +16,8 @@ def main():
 		for (name, value) in row.items():
 			value = value.decode("utf-8", "replace")
 			#will add try statement here
-			#result = execute_validation_fns(name, value)
-			value = value.encode('utf-8')
-			row[name] = value
+			result = execute_validation_fns(name, value)
+			row[name] = result
 			
 		print row
 			
@@ -32,53 +32,48 @@ def execute_validation_fns(header, value):
 		'FooDuration': format_duration,
 		'BarDuration': format_duration,
 		'TotalDuration': update_total_duration,
-		'Note': unicode_replacement #this won't work
+		'Note': noop #this won't work, need a fn
 	}
-
-	fn = names_to_fns.get(header, "Invalid header")
+	fn = names_to_fns.get(header, noop)
 	return fn(value)
 		
-def format_timestamp():
+def format_timestamp(ts):
+	ts_as_date_et = datetime.strptime(ts, "%m/%d/%y %H:%M:%S %p")
+	ts_as_date_pt = ts_as_date_et - timedelta(hours=3)
 
-	return
-	#The Timestamp column should be formatted in ISO-8601 format.
-	#date.isoformat()
-	#The Timestamp column should be assumed to be in US/Pacific time;
-  	#please convert it to US/Eastern.
-  	#date_str = "2009-05-05 22:28:15"
-#datetime_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
-#datetime_obj_utc = datetime_obj.replace(tzinfo=timezone('UTC'))
-#datetime_obj_utc.astimezone(timezone('US/Pacific'))
+	return datetime.strftime(ts_as_date_pt, "%m/%d/%y %H:%M:%S %p")
   	
-
-def format_zipcode():
-	return
+def format_zipcode(z):
+	if (len(z) < 5):
+		missing = 5 - len(z)
+		z = "0"*missing + z
+	return z
 	#All ZIP codes should be formatted as 5 digits. If there are less
   #than 5 digits, assume 0 as the prefix.
 
-def format_name():
+def format_name(name):
 	return
 	#All name columns should be converted to uppercase. There will be
   #non-English names.
 
-def format_address():
+def format_address(addr):
 	return
 	#The Address column should be passed through as is, except for
   #Unicode validation. 
 
-def format_duration():
+def format_duration(dur):
 	return
 #The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
   #format (where MS is milliseconds); please convert them to a floating
   #point seconds format.
 
-def update_total_duration():
+def update_total_duration(dur):
 	return
 	#The column "TotalDuration" is filled with garbage data. For each
  # row, please replace the value of TotalDuration with the sum of
   #FooDuration and BarDuration.
 
-def unicode_replacement():
+def noop(rep):
 	return
 	#"a\x81b".decode("utf-8", "replace")
 	#The column "Notes" is free form text input by end-users; please do
